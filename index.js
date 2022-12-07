6e99a3f289081e7c26c12ec5073fd9d398838fc4
Ontaria Woods <ontaria.woods@gmail.com>
2022-12-07 06:25:41 -0500

    dom added to second button

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const firstForm = document
   .addEventListener("submit", (e) => {
     e.preventDefault();
     console.log("working", e);
+    fetch(
+      `https://thecocktaildb.com/api/json/v1/1/search.php?f=${e.target[0].value}`
+    ).then((response) =>
+      response.json().then((response) => console.log("response", response))
+    );
   });
 
 const secondForm = document
@@ -16,7 +21,9 @@ const secondForm = document
       .then((resp) => resp.json())
       .then((resp) =>
         resp.drinks.map((drink) => {
+          const drinkList = document.getElementById("drinks");
           const li = document.createElement("li");
+          li.id = "different-drinks";
           const h2 = document.createElement("h2");
           const img = document.createElement("img");
           const div = document.createElement("div");
