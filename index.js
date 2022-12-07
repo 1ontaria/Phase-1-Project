c7348f389531331affc539db6b2ec9177e66730c
Ontaria Woods <ontaria.woods@gmail.com>
2022-12-07 07:07:28 -0500

    both forms are working

--- index.js
+++ index.js
@@ -9,23 +9,11 @@ const firstForm = document
     console.log("working", e);
     fetch(
       `https://thecocktaildb.com/api/json/v1/1/search.php?f=${e.target[0].value}`
-    ).then((response) =>
-      response.json().then((response) => console.log("response", response))
-    );
-  });
-
-const secondForm = document
-  .getElementById("alcohol")
-  .addEventListener("submit", (e) => {
-    e.preventDefault();
-    console.log("working", e);
-    fetch(
-      `https://thecocktaildb.com/api/json/v1/1/filter.php?i=${e.target[0].value}`
     )
-      .then((resp) => resp.json())
-      .then((resp) =>
-        resp.drinks.map((drink) => {
-          const drinkList = document.getElementById("drinks");
+      .then((response) => response.json())
+      .then((response) =>
+        response.drinks.map((drink) => {
+          const drinkList = document.getElementById("more-drinks");
           const li = document.createElement("li");
           li.id = "different-drinks";
           const h2 = document.createElement("h2");
@@ -33,9 +21,6 @@ const secondForm = document
           const div = document.createElement("div");
           div.id = "hover";
           div.textContent = "drink responsibily";
-          img.addEventListener("click", () => {
-            alert("drink responsibily");
-          });
           img.src = drink.strDrinkThumb;
           h2.textContent = drink.strDrink;
           li.append(h2, img);
@@ -43,3 +28,37 @@ const secondForm = document
         })
       );
   });
+
+function alcoholDrink(e) {
+  e.preventDefault();
+  //console.log("working", e);
+  fetch(
+    `https://thecocktaildb.com/api/json/v1/1/filter.php?i=${e.target[0].value}`
+  )
+    .then((resp) => resp.json())
+    .then((resp) =>
+      resp.drinks.map((drink) => {
+        const drinkList = document.getElementById("drinks");
+        const li = document.createElement("li");
+        li.id = "different-drinks";
+        const h2 = document.createElement("h2");
+        const img = document.createElement("img");
+        const div = document.createElement("div");
+        div.id = "hover";
+        div.textContent = "drink responsibily";
+        img.addEventListener("click", () => {
+          alert("drink responsibily");
+        });
+        img.src = drink.strDrinkThumb;
+        h2.textContent = drink.strDrink;
+        li.append(h2, img);
+        drinkList.append(li);
+      })
+    )
+    .catch(() => {
+      alert("That won't work!");
+    });
+}
+const secondForm = document
+  .getElementById("alcohol")
+  .addEventListener("submit", alcoholDrink);
