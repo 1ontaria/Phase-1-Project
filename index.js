06babe17f1306b6231bf67add196d06158bdc27a
Ontaria Woods <ontaria.woods@gmail.com>
2022-12-09 10:37:55 -0500

    made DOM manipulation more concise

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
-document.addEventListener("DOMContentLoaded", () => {
-  alert("Welcome! Please drink responisibly!");
-});
+document.addEventListener("DOMContentLoaded", pageLoadAlert());
 
 const firstForm = document
   .getElementById("letter")
-  .addEventListener("submit", (e) => {
+  .addEventListener("submit", initialDrink);
+
+const secondForm = document
+  .getElementById("alcohol")
+  .addEventListener("submit", alcoholDrink);
+
+function pageLoadAlert() {
+  alert("Welcome! Please drink responisibly!");
+}
+
+function initialDrink(e) {
+  {
     e.preventDefault();
     console.log("working", e);
     fetch(
@@ -13,7 +22,10 @@ const firstForm = document
       .then((response) => response.json())
       .then((response) =>
         response.drinks.map((drink) => {
-          const initialList = document.getElementById("more-drinks");
+          // const initialList = document.getElementById("more-drinks");
+          const ul = document.createElement("ul");
+          ul.id = "drinks";
+          const drinkDiv = document.getElementById("drinkContainer");
           const li = document.createElement("li");
           li.id = "different-drinks";
           const h2 = document.createElement("h2");
@@ -24,52 +36,46 @@ const firstForm = document
           img.src = drink.strDrinkThumb;
           h2.textContent = drink.strDrink;
           li.append(h2, img);
-          initialList.append(li);
+          ul.append(li);
+          drinkDiv.append(ul);
         })
       );
     e.target[0].value = "";
-  });
+  }
+}
 
 function alcoholDrink(e) {
-  const drinkList = document.getElementById("drinks");
-  drinkList.innerText = "";
+  // const drinkList = document.getElementById("drinks");
+
   e.preventDefault();
   //console.log("working", e);
   fetch(
     `https://thecocktaildb.com/api/json/v1/1/filter.php?i=${e.target[0].value}`
   )
     .then((resp) => resp.json())
-    .then(
-      (resp) =>
-        resp.drinks.map((drink) => {
-          const li = document.createElement("li");
-          li.id = "different-drinks";
-          const h2 = document.createElement("h2");
-          const img = document.createElement("img");
-          const div = document.createElement("div");
-          div.id = "hover";
-          div.textContent = "drink responsibily";
-          img.addEventListener("click", () => {
-            fetch(
-              "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?iid=552"
-            )
-              .then((resp) => resp.json())
-              .then((resp) =>
-                resp.drinks.forEach(drink => {
-                ((drink) => {
-                  alert(drink.idDrink);
-                })
-              );
-          });
-          img.src = drink.strDrinkThumb;
-          h2.textContent = drink.strDrink;
-          li.append(h2, img);
-          drinkList.append(li);
-        }),
-      (e.target[0].value = "")
-    );
+    .then((resp) => {
+      const ul = document.createElement("ul");
+      ul.id = "drinks";
+      const drinkDiv = document.getElementById("drinkContainer");
+      drinkDiv.innerHTML = "";
+      resp.drinks.forEach((drink) => {
+        const li = document.createElement("li");
+        li.className = "different-drinks";
+        const h2 = document.createElement("h2");
+        const img = document.createElement("img");
+        img.addEventListener("click", () => {
+          console.log("hey!");
+        });
+        img.src = drink.strDrinkThumb;
+        h2.textContent = drink.strDrink;
+        li.append(h2, img);
+        ul.append(li);
+      });
+      drinkDiv.append(ul);
+      e.target[0].value = "";
+    });
 }
 
-const secondForm = document
-  .getElementById("alcohol")
-  .addEventListener("submit", alcoholDrink);
+// function refreshOnSubmit() {
+//   const cheersButton = document.getElementById("drink-up");
+// }
