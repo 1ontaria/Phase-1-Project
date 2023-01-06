4fe18c4a43918ad55ecaf2ed97bd72aa8460dbf2
Ontaria Woods <ontaria.woods@gmail.com>
2023-01-06 16:22:09 -0500

    added page alert, and began working on the functionality of the first button

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 document.addEventListener("DOMContentLoaded", pageLoadAlert());
+
+function pageLoadAlert() {
+  alert(`Welcome! By clicking "OK", you confirm you are over the age of 21.`);
+}
+
+const firstForm = document
+  .getElementById("letter")
+  .addEventListener("submit", initialShot);
+
+function initialShot(e) {
+  e.preventDefault();
+  console.log("working", e);
+}
