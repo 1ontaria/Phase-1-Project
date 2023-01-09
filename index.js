909b84d4f03909cadfb7d00bd87d3e615e3fee75
Ontaria Woods <ontaria.woods@gmail.com>
2023-01-09 13:28:03 -0500

    restarting showIngredient function

--- index.js
+++ index.js
@@ -8,23 +8,22 @@ const firstForm = document
   .getElementById("letter")
   .addEventListener("submit", initialShot);
 
-const ul = document.createElement("ul");
-ul.id = "drinks";
-const drinkDiv = document.getElementById("drinkContainer");
-drinkDiv.innerHTML = "";
-
 function initialShot(e) {
   e.preventDefault();
   console.log("working", e);
+  const ul = document.createElement("ul");
+  ul.id = "drinks";
+  const drinkDiv = document.getElementById("drinkContainer");
+  drinkDiv.innerHTML = "";
   fetch(
-    `http://www.thecocktaildb.com/api/json/v1/1/search.php?f=${e.target[0].value}`
+    `http://www.thecocktaildb.com/api/json/v1/1/search.php?f=${e.target[0].value}`,
+    { credentials: "same-origin" }
   )
     .then((response) => response.json())
     .then((response) => {
       response.drinks.forEach((drink) => {
         const h2 = document.createElement("h2");
         h2.textContent = drink.strDrink;
-
         h2.addEventListener("click", (e) => showIngredients(drink.strDrink, e));
 
         const li = document.createElement("li");
@@ -39,21 +38,4 @@ function initialShot(e) {
     });
 
   drinkDiv.append(ul);
-}
-
-function showIngredients(drinkName, e) {
-  console.log(e);
-  fetch(`http://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drinkName}`)
-    .then((response) => response.json())
-    .then((response) => {
-      response.drinks.forEach((drink) => {
-        const drinkInstructions = [
-          drink.strIngredient1,
-          drink.strIngredient2,
-          drink.strIngredient3,
-          drink.strIngredient4,
-          drink.strIngredient5,
-        ];
-      });
-    });
 }
